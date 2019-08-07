4bd0bfb1adb6d08adad4317869ddff66d63c9cf2
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2019-08-07 22:29:11 +0530

    game complete

--- app.js
+++ app.js
@@ -8,28 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, activePlayer,roundScore;
-scores = [0,0];
-activePlayer = 0;
-roundScore=0;
+var scores, activePlayer,roundScore,gamePlaying;
+const player = {
+    one:0,
+    two:1
+};
+newGame();
 
-/*document.querySelector("#current-"+activePlayer).textContent = dice;//using textcontent we can only modify text contents. 
-/*document.queryselector("#current-"+activePlayer).innerhtml = '<em>'+dice+'</em>';//innerHTML is used so that we can change html elements too.
-*/
-document.querySelector(".dice").style.display="none";
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
 
 /*function btn(){
 
 }
 document.querySelector("btn-roll").addEventListener("click",btn);//either we can write in this format by defining function elsewhere .(see that function is only passed to the callback function instead of us calling it: we have put btn instead of btn()  )
 */
+
 /*CALLBACK FUNCTION: btn is a callback function ie it is not called by us but by another funtion.
 */
 document.querySelector(".btn-roll").addEventListener("click",function(){ //ANONYMOYS FUNCTION: the fuction defined here cannot be called by anyone else as it doesn't have a name.
+if(gamePlaying){
 //1.Random number
 var dice = Math.floor(Math.random()*6+1);
 //2.Display the result
@@ -39,35 +35,85 @@ btnDom.src="dice-" + dice + ".png";
 //3.Update the score if the outcome is not 1
 if(dice !== 1){
     roundScore += dice;
-    console.log(roundScore);
-    document.querySelector("#current-" + activePlayer).textContent = roundScore; 
-   document.querySelector(".btn-hold").addEventListener("click",function(){
-       scores[activePlayer] += roundScore;
-       roundScore=0;
-       console.log(scores);
-       document.getElementById("score-"+activePlayer).textContent = scores[activePlayer];
-       if(activePlayer === 1){
-           activePlayer = 0;
-       }
-       else{
-           activePlayer = 1;
-       }
-       })
+    document.querySelector("#current-" + activePlayer).textContent = roundScore; }
+else{
+    //Next player
+    nextPlayer();
+  
 
 }
-else{
+}
+}) ;
+//Hold
+document.querySelector(".btn-hold").addEventListener("click",function(){
+    if(gamePlaying){
+    //1.Add current score to the global score   
+    scores[activePlayer] += roundScore;
+    //2.Update UI
+    document.getElementById("score-"+activePlayer).textContent = scores[activePlayer];
+    //3.Check if the current player won.
+    if (scores[activePlayer]>= 100){
+        document.querySelector(".dice").style.display="none";
+        document.getElementById("name-"+activePlayer).textContent="Winner!";
+        document.querySelector(".player-"+ activePlayer + "-panel").classList.add("winner"); 
+        document.querySelector(".player-"+ activePlayer + "-panel").classList.remove("active"); 
+        //document.querySelector(".btn-roll").style.display="none";
+        //document.querySelector(".btn-hold").style.display="none";
+        /*We can either define the stoping of game afer winning by defining a state variable or by setting the roll and hold button display to none*/
+    gamePlaying= false;
+    }
 
-    roundScore = 0;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore; 
-    if(activePlayer===1){
-        activePlayer = 0;
+else{
+    //4.Next player
+
+       nextPlayer();
+}
+
+  }  },false);
+    document.querySelector(".btn-new").addEventListener("click",newGame);
+
+    function nextPlayer(){
+    if(activePlayer === player.one){
+        activePlayer = player.two;
     }
     else{
-        activePlayer = 1;
+        activePlayer = player.one;
     }
+    roundScore = 0;
+    document.getElementById("current-0").textContent= 0 ;
+    document.getElementById("current-1").textContent= 0 ; 
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    //document.querySelector(".player-0-panel").classList.add(".active");
+    //document.querySelector(".player-1-panel").classList.remove(".active");
 
 }
+function newGame(){
+scores = [0,0]; 
+activePlayer = player.one;
+roundScore=0;
+gamePlaying = true;
+
+/*document.querySelector("#current-"+activePlayer).textContent = dice;//using textcontent we can only modify text contents. 
+/*document.queryselector("#current-"+activePlayer).innerhtml = '<em>'+dice+'</em>';//innerHTML is used so that we can change html elements too.
+*/
+document.querySelector(".dice").style.display="none";
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+document.getElementById("name-"+player.one).textContent="Player 1";
+document.getElementById("name-"+player.two).textContent="Player 2";
+document.querySelector(".player-0-panel").classList.remove("winner");
+document.querySelector(".player-1-panel").classList.remove("winner");
+document.querySelector(".player-0-panel").classList.remove("active");
+document.querySelector(".player-1-panel").classList.remove("active");
+document.querySelector(".player-0-panel").classList.add("active");
+
+//document.querySelector(".btn-roll").style.display="inline-block";
+//document.querySelector(".btn-hold").style.display="inline-block";
 
 
 
-}) ;
+
+}
